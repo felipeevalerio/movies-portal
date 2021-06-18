09ed9c9d2d0eb2d2f430b6b1e36384770d041486
felipe-spindola <felipeevalerio@outlook.com>
2021-06-18 09:59:36 -0300

    card-movie

--- src/api.js
+++ src/api.js
@@ -23,9 +23,27 @@ async function showPopularMovies(){
 function createMovieDiv(movies){
     for(let movie of movies){
         const movieDiv = document.createElement("div");
-        movieDiv.innerHTML = `<a href="movie.html"><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ljPHd7WiPVKmuXi1hgQUpZQslbC.jpg" alt="Imagem de ${movie.original_title}"></a>`
+        const movieInfo = createMovieInfo(movie);
+        movieDiv.innerHTML = movieInfo;
         movieDiv.classList.add("movie");
         moviesSection.appendChild(movieDiv);
     }
 }
 
+
+function createMovieInfo(movie){
+const movieInfo = `<a href="movie.html">
+    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ljPHd7WiPVKmuXi1hgQUpZQslbC.jpg" alt="Imagem de ${movie.original_title}">
+    <div class="movie-info">
+        <h2>${movie.original_title}</h2>
+        <div class="date-rate">
+            <p>${movie.release_date}</p>
+            <div class="rate">
+                <img class="star" src="../public/assets/star.svg" alt="Nota de Avaliação"/><p>${movie.vote_average}</p>
+            </div>
+        </div>
+    </div>
+</a>`
+
+return movieInfo;
+}
